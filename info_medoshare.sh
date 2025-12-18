81154e5469bdd51bd49734279c1e69c075e70104
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2025-12-18 21:39:48 +0200

    Add files via upload

---
+++ info_medoshare.sh
@@ -0,0 +1,26 @@
+echo ">>>> MedoShare Server <<<<"
+sleep 3
+echo "> report for Server"
+sleep 3s
+
+echo " "
+echo "> To contact WhatsApp"
+sleep 2s
+
+echo "> م . أحمد عمرو "
+echo "> 01068512690 "
+echo "> اسعار الاشتراك داخل مصر "
+echo "> سنوى 70 جنيه"
+echo "> تواصل علي الواتساب "
+sleep 2s
+
+echo " "
+echo "> 18-12-25"
+sleep 2s
+
+echo "> Medoshare Server oscam No.1 & No.3 Test 7 days Free 18-12-2025 updated"
+echo "> Medoshare Server Ncam No.2 & No.4 Test 7 days Free 18-12-2025 updated"
+sleep 2s
+
+echo ">>> Last Updatting 18-12-2025 <<<"
+exit 0
