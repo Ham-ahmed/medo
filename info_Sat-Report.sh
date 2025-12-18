e235d42fdbfd151b6d960b263365704593810a4e
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2025-12-18 21:52:47 +0200

    Add files via upload

---
+++ info_Sat-Report.sh
@@ -0,0 +1,128 @@
+echo ">>>> Medoshare Server <<<<"
+sleep 3
+echo "> report for Satallite"
+sleep 3s
+
+echo " "
+echo "> Hispasat 30W"
+sleep 2s
+
+echo "> MEO TV البافة بالكامل"
+echo "> Nos جميع القنوات"
+echo "> MEO TV الباقة بالكامل"
+sleep 2s
+
+echo " "
+echo "> Thor 1W"
+sleep 2s
+
+echo "> Focus Sat الباقة بالكامل updated"
+echo "> UPC Direct كل القنوات تعمل"
+echo "> Digi TV الباقة بالكامل"
+echo "> Canal Digital جميع القنوات"
+echo "> Allent الباقة بالكامل"
+sleep 2s
+
+echo " "
+echo "> Bulgaria-Sat 1.9E"
+sleep 2s 
+
+echo "> Neosat الباقة بالكامل"
+sleep 2s
+
+echo " "
+echo "> Amos 4W"
+sleep 2s
+
+echo "> اقة YES بالكامل SD,HD,4K"
+sleep 2s
+
+echo " "
+echo "> Eutlsat 9E"
+sleep 2s
+
+echo "> discovery الالمانية المترجمه للعربية حصريا كامله"
+echo "> AFN كامله تعمل علي أجهزة الجيل الثالث"
+echo "> Mtva الباقة المجرية كاملة"
+sleep 2s
+
+echo " "
+echo "> Hotbird 13E"
+sleep 2s
+
+echo "> Eurosport القنوات الانجليزية"
+echo "> NC+ بالكامل من ضمنها قنوات Eleven - Canal+"
+echo "> Polsat Sport Premium و Cyfrowy Polsat قنوات باقة"
+echo "> Mediaset تعمل بالكامل باقة"
+echo "> SRG SSR تعمل بالكامل باقةالسويسرية"
+echo "> RAI القنوات الايطالية بالكامل"
+echo "> Love Nature 4k و My zen 4K و Museum 4K تعمل بامتياز"
+echo "> AB الباقة الفرنسية Action وقناة"
+echo "> HBO كامل القنوات"
+echo "> VOD كافة قنوات باقة"
+echo "> Travelxp 4k قناة"
+echo "> vivacom قنوات"
+sleep 2s
+
+echo " "
+echo "> Eutelsat 16E"
+sleep 2s
+
+echo "> MAX TV الباقة الكرواتية"
+echo "> Antiksat جميع قنوات باقة"
+echo "> Pink باقة جميغ قنوات"
+echo "> HRT الباقة الكرواتية"
+echo "> Hayat HD فنوات باقة"
+echo "> Freesat Romania و Sport.Ro و Pro TV و Kanal D Romania وقنوات TVR"
+sleep 2s
+
+echo " "
+echo "> Astra 19.2E"
+sleep 2s
+
+echo "> SKYDE الباقة الالمانية"
+echo "> + Astra قنوات"
+echo "> HD+ قنوات باقة"
+echo "> TNT قنوات باقة"
+echo "> ORF  قنوات باقة"
+sleep 2s
+
+echo " "
+echo "> Astra 23E"
+sleep 2s
+
+echo "> CanalDigitaal قنوات"
+echo "> Skylink قنوات باقة"
+echo "> skylink قنوات باقة"
+sleep 2s
+
+echo " "
+echo "> Hellasat 39E"
+sleep 2s
+
+echo "> orang romania قنوات باقة"
+sleep 2s
+
+echo " "
+echo "> TurkSat 42E"
+sleep 2s
+
+echo "> Dsmart واغلب قنوات القمر"
+sleep 2s
+
+echo " "
+echo "> Express-80"
+sleep 2s
+
+echo "> Telekarta وتعمل اغلب القمر"
+sleep 2s
+
+echo " "
+echo "> Turkmani + yahsat 52E"
+sleep 2s
+
+echo "> Big BANG تعمل بامتياز"
+sleep 2s
+
+echo ">>> Last Updatting 18-12-2025 <<<"
+exit 0
